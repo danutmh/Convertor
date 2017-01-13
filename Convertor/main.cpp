9b1c1dc87ead0b84f7516d08739fac37682f78b5
danutmh <danutmh@yahoo.com>
2017-01-13 22:08:44 +0200

    Au fost adaugate functiile de arie si viteza

--- Convertor/main.cpp
+++ Convertor/main.cpp
@@ -8,6 +8,11 @@ short Ucurent,Uconversie;
  double Densitatea(short UCurent,short UConversie, double a);
  double Timp(short UCurent,short UConversie, double a);
  double Presiune(short UCurent,short UConversie, double a);
+ double Lungime(short UCurent,short UConversie, double a);
+ double Arie(short UCurent,short UConversie,double a);
+ double Viteza(short UCurent,short UConversie, double a);
+ double Masa(short UCurent,short UConversie, double a);
+
 
 void Meniu()
 {short select,c,dc; double a;
@@ -28,55 +33,91 @@ cout<<"Selectati unitatea de conversie:";cin>>select;system("CLS");
     switch (select){
 
 case 1:
+    lungime:
 cout<<"0)Meniu anterior"<<endl;
 cout<<"1)Milimetri"<<endl;
 cout<<"2)Centimetri"<<endl;
-cout<<"3)Metri"<<endl;
-cout<<"4)Kilometri"<<endl;
-cout<<"5)Tol"<<endl;
-cout<<"6)Piciare"<<endl;
-cout<<"7)Yarzi"<<endl;
-cout<<"8)Mile"<<endl;
-cout<<"9)Mile Navale"<<endl;
+cout<<"3)Decimetri"<<endl;
+cout<<"4)Metri"<<endl;
+cout<<"5)Decametri"<<endl;
+cout<<"6)Hectometri"<<endl;
+cout<<"7)Kilometri"<<endl;
+cout<<"8)Tol"<<endl;
+cout<<"9)Picioare"<<endl;
+cout<<"10)Yarzi"<<endl;
+cout<<"11)Mile"<<endl;
+cout<<"12)Mile Navale"<<endl;
+cout<<"Selectati unitatea curenta:";cin>>c;cout<<endl;
+    if(c==0)
+    {system("CLS");
+    goto menu;}
+    if(c<0||c>13)
+    {system("CLS");
+        goto lungime;}
+cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
+if(dc==0)
+    {system("CLS");
+    goto menu;}
+    if(dc<0||dc>13)
+{system("CLS");
+        goto lungime;}
+cout<<"Cantitatea:";cin>>a;cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Lungime(c,dc,a);cout<<endl;
 cin.get();
-    system("CLS");
-    goto menu;
+cin.get();
+system("CLS");
+goto lungime;
 
 
 case 2:
+    arie:
 cout<<"0)Meniu anterior"<<endl;
 cout<<"1)Milimetri patrati"<<endl;
 cout<<"2)Centimetri patrati"<<endl;
 cout<<"3)Decimetri patrati"<<endl;
 cout<<"4)Metri patrati"<<endl;
-cout<<"5)Decametri patrati"<<endl;
+cout<<"5)Ari"<<endl;
 cout<<"6)Hectari"<<endl;
 cout<<"7)Kilometri patrati"<<endl;
-cout<<"8)Ari"<<endl;
-cout<<"9)Acri"<<endl;
-cout<<"10)Picioare patrate"<<endl;
-cout<<"11)Yarzi patrati"<<endl;
-cout<<"12)Mile patrate"<<endl;
-cout<<"Alegeti unitatea curenta:"<<endl;
-cout<<"Alegeti unitatea in care doriti sa efectuati cobversia:"<<endl;
+cout<<"8)Toli patrati"<<endl;
+cout<<"9)Picioare patrate"<<endl;
+cout<<"10)Yarzi patrati"<<endl;
+cout<<"11)Mile patrate"<<endl;
+cout<<"12)Acrii"<<endl;
+cout<<"Selectati unitatea curenta:";cin>>c;cout<<endl;
+    if(c==0)
+    {system("CLS");
+    goto menu;}
+    if(c<0||c>12)
+    {system("CLS");
+        goto arie;}
+cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
+if(dc==0)
+    {system("CLS");
+    goto menu;}
+    if(dc<0||dc>12)
+{system("CLS");
+        goto arie;}
+cout<<"Cantitatea:";cin>>a;cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Arie(c,dc,a);cout<<endl;
 cin.get();
-    system("CLS");
-    goto menu;
+cin.get();
+system("CLS");
+goto arie;
+
 
 case 3:
 cout<<"0)Meniu anterior"<<endl;
-cout<<"1)Milimetri patrati"<<endl;
-cout<<"2)Centimetri patrati"<<endl;
-cout<<"3)Decimetri patrati"<<endl;
-cout<<"4)Metri patrati"<<endl;
-cout<<"5)Decametri patrati"<<endl;
-cout<<"6)Hectari"<<endl;
-cout<<"7)Kilometri patrati"<<endl;
-cout<<"8)Ari"<<endl;
-cout<<"9)Acri"<<endl;
-cout<<"10)Picioare patrate"<<endl;
-cout<<"11)Yarzi patrati"<<endl;
-cout<<"12)Mile patrate"<<endl;
+cout<<"1)Milimetri cubi"<<endl;
+cout<<"2)Centimetri cubi"<<endl;
+cout<<"3)Decimetri cubi"<<endl;
+cout<<"4)Metri cubi"<<endl;
+cout<<"5)Decametri cubi"<<endl;
+cout<<"6)Hectometri cubi"<<endl;
+cout<<"7)Kilometri cubi"<<endl;
+cout<<"10)Picioare cube"<<endl;
+cout<<"11)Yarzi cubi"<<endl;
+cout<<"12)Mile cube"<<endl;
 cin.get();
     system("CLS");
     goto menu;
@@ -105,22 +146,40 @@ cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
     {system("CLS");
         goto timp;}
 cout<<"Cantitatea:";cin>>a;cout<<endl;
-cout<<"Rezultatul este:"<<Timp(c,dc,a);cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Timp(c,dc,a);cout<<endl;
 cin.get();
 cin.get();
 system("CLS");
 goto timp;
 
 case 5:
+    viteza:
     cout<<"0)Meniu anterior"<<endl;
     cout<<"1)Metri/secunda"<<endl;
     cout<<"2)Kilometri/secunda"<<endl;
     cout<<"3)Kilometri/ora"<<endl;
-    cout<<"4)Mile/ora"<<endl;
-    cout<<"5)Picioare/secunda"<<endl;
-    cin.get();
-    system("CLS");
-    goto menu;
+    cout<<"4)Picioare/secunda"<<endl;
+    cout<<"5)Mile/ora"<<endl;
+    cout<<"Selectati unitatea curenta:";cin>>c;cout<<endl;
+    if(c==0)
+    {system("CLS");
+    goto menu;}
+    if(c<0||c>5)
+    {system("CLS");
+        goto viteza;}
+cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
+if(dc==0)
+    {system("CLS");
+    goto menu;}
+    if(dc<0||dc>5)
+{system("CLS");
+        goto viteza;}
+cout<<"Cantitatea:";cin>>a;cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Viteza(c,dc,a);cout<<endl;
+cin.get();
+cin.get();
+system("CLS");
+goto viteza;
 case 6:
     temperatura:
 cout<<"0)Meniu anterior"<<endl;
@@ -142,7 +201,7 @@ cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
     {system("CLS");
         goto temperatura;}
 cout<<"Cantitatea:";cin>>a;cout<<endl;
-cout<<"Rezultatul este:"<<Temperatura(c,dc,a);cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Temperatura(c,dc,a);cout<<endl;
 cin.get();
 cin.get();
 system("CLS");
@@ -150,6 +209,7 @@ goto temperatura;
 
 
 case 7:
+    masa:
     cout<<"0)Meniu anterior"<<endl;
     cout<<"1)Miligrame"<<endl;
     cout<<"2)Centigrame"<<endl;
@@ -161,9 +221,26 @@ case 7:
     cout<<"8)Tone"<<endl;
     cout<<"9)Uncie"<<endl;
     cout<<"10)Livre"<<endl;
+cout<<"Selectati unitatea curenta:";cin>>c;cout<<endl;
+if(c==0)
+    {system("CLS");
+    goto menu;}
+    if(c<0||c>10)
+    {system("CLS");
+        goto masa;}
+cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
+    if(dc==0)
+    {system("CLS");
+    goto menu;}
+    if(dc<0||dc>10)
+    {system("CLS");
+        goto masa;}
+cout<<"Cantitatea:";cin>>a;cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Masa(c,dc,a);cout<<endl;
 cin.get();
-    system("CLS");
-    goto menu;
+cin.get();
+system("CLS");
+goto masa;
 
 case 8:
     cout<<"0)Meniu anterior"<<endl;
@@ -201,7 +278,7 @@ cout<<"Selectati unitatea in care doriti sa transformati:";cin>>dc;cout<<endl;
     {system("CLS");
         goto presiune;}
 cout<<"Cantitatea:";cin>>a;cout<<endl;
-cout<<"Rezultatul este:"<<Presiune(c,dc,a);cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Presiune(c,dc,a);cout<<endl;
 cin.get();
 cin.get();
 system("CLS");
@@ -233,7 +310,7 @@ if(dc==0)
 {system("CLS");
         goto densitate;}
 cout<<"Cantitatea:";cin>>a;cout<<endl;
-cout<<"Rezultatul este:"<<Densitatea(c,dc,a);cout<<endl;
+cout<<"Rezultatul este:"<<setprecision(20)<<fixed<<Densitatea(c,dc,a);cout<<endl;
 cin.get();
 cin.get();
 system("CLS");
@@ -376,6 +453,196 @@ else if(UCurent==3&&UConversie==2)
     return a*760;
 
 }
+double Lungime(short UCurent,short UConversie, double a)
+{double b=0;
+if(UCurent>0&&UCurent<8&&UConversie>0&&UConversie<8)
+return a*pow(10,UCurent-UConversie);
+if(UCurent>0&&UCurent<8&&UConversie>7&&UConversie<13)
+{b=a*pow(10,UCurent-4);
+if(UConversie==8)
+    return b*393700787/pow(10,7);
+if(UConversie==9)
+    return b*32808399/pow(10,7);
+if(UConversie==10)
+    return b*10936133/pow(10,7);
+if(UConversie==11)
+    return b*621371192/pow(10,12);
+if(UConversie==12)
+    return b*539956803/pow(10,12);
+}
+if(UCurent>7&&UCurent<13&&UConversie>0&&UConversie<8)
+{if(UCurent==8)
+b=a*0.0254;
+if(UCurent==9)
+b=a*0.3048;
+if(UCurent==10)
+b=a*0.9144;
+if(UCurent==11)
+b=a*1609.344;
+if(UCurent==12)
+b=a*1852;
+return b*pow(10,UConversie-4);
+
+}
+if(UCurent>7&&UCurent<13&&UConversie>7&&UConversie<13)
+{if(UCurent==8)
+b=a*0.0254;
+if(UCurent==9)
+b=a*0.3048;
+if(UCurent==10)
+b=a*0.9144;
+if(UCurent==11)
+b=a*1609.344;
+if(UCurent==12)
+b=a*1852;
+if(UConversie==8)
+    return b*393700787/pow(10,7);
+if(UConversie==9)
+    return b*32808399/pow(10,7);
+if(UConversie==10)
+    return b*10936133/pow(10,7);
+if(UConversie==11)
+    return b*621371192/pow(10,12);
+if(UConversie==12)
+    return b*539956803/pow(10,12);
+
+
+}
+
+}
+double Arie(short UCurent,short UConversie,double a)
+{long double b=0;
+ if(UCurent>0&&UCurent<8&&UConversie>0&&UConversie<8)
+ return a*pow(100,UCurent-UConversie);
+
+ if(UCurent>0&&UCurent<8&&UConversie<13&&UConversie>7)
+ {b=a*pow(pow(10,UCurent-4),2);
+     if(UConversie==8)
+    return b*pow(393700787/pow(10,7),2);
+if(UConversie==9)
+    return b*pow(32808399/pow(10,7),2);
+if(UConversie==10)
+    return b*pow(10936133/pow(10,7),2);
+if(UConversie==11)
+    return b*pow(621371192/pow(10,12),2);
+if(UConversie==12)
+    return b*pow(247105381/pow(10,12),2);
+ }
+if(UConversie>0&&UConversie<8&&UCurent<13&&UCurent>7)
+{if(UCurent==8)
+b=a*0.00064516;
+if(UCurent==9)
+b=a*0.09290304;
+if(UCurent==10)
+b=a*0.83612736;
+if(UCurent==11)
+b=a*2589988.11;
+if(UCurent==12)
+b=a*4046.85642;
+return b*pow(100,UConversie-4);
+}
+if(UConversie>7&&UConversie<13&&UCurent<13&&UCurent>7)
+if(UCurent==8)
+b=a*0.00064516;
+if(UCurent==9)
+b=a*0.09290304;
+if(UCurent==10)
+b=a*0.83612736;
+if(UCurent==11)
+b=a*2589988.11;
+if(UCurent==12)
+b=a*4046.85642;
+if(UConversie==8)
+    return b*pow(393700787/pow(10,7),2);
+if(UConversie==9)
+    return b*pow(32808399/pow(10,7),2);
+if(UConversie==10)
+    return b*pow(10936133/pow(10,7),2);
+if(UConversie==11)
+    return b*pow(621371192/pow(10,12),2);
+if(UConversie==12)
+    return b*pow(247105381/pow(10,12),2);
+}
+double Viteza(short UCurent,short UConversie, double a)
+{if(UCurent==1&&UConversie==2)
+    return a/1000;
+ if(UCurent==1&&UConversie==3)
+    return a/1000*3600;
+ if(UCurent==1&&UConversie==4)
+    return a*3.2808399;
+ if(UCurent==1&&UConversie==5)
+    return a*2.23693629;
+ if(UCurent==2&&UConversie==1)
+    return a*1000;
+ if(UCurent==2&&UConversie==3)
+    return a*3600;
+ if(UCurent==2&&UConversie==4)
+    return a*3280.8399;
+ if(UCurent==2&&UConversie==5)
+    return a*2236.93629;
+ if(UCurent==3&&UConversie==1)
+    return a*0.277777778;
+ if(UCurent==3&&UConversie==2)
+    return a*277777778/pow(10,12);
+ if(UCurent==3&&UConversie==4)
+    return a*0.911344415;
+ if(UCurent==3&&UConversie==5)
+    return a*0.621371192;
+ if(UCurent==4&&UConversie==1)
+    return a*0.3048;
+ if(UCurent==4&&UConversie==2)
+    return a*0.0003048;
+ if(UCurent==4&&UConversie==3)
+    return a*1.09728;
+ if(UCurent==4&&UConversie==5)
+    return a*0.681818182;
+ if(UCurent==5&&UConversie==1)
+    return a*0.44704;
+ if(UCurent==5&&UConversie==2)
+    return a*0.00044704;
+ if(UCurent==5&&UConversie==3)
+    return a*1.609344;
+ if(UCurent==5&&UConversie==4)
+    return a*1.46666667;
+
+}
+double Masa(short UCurent,short UConversie, double a)
+{double b=0;
+ if(UCurent<8&&UConversie<8)
+ return a*pow(10,UCurent-UConversie);
+ if(UCurent<8&&UConversie==8)
+ {b=a*pow(10,UCurent-4);
+  return b/1000000;}
+  if(UCurent<8&&UConversie==9)
+ {b=a*pow(10,UCurent-4);
+  return b*352739619/pow(10,10);}
+  if(UCurent<8&&UConversie==10)
+ {b=a*pow(10,UCurent-4);
+  return b*220462262/pow(10,11);}
+  if(UCurent==8&&UConversie<8)
+  {b=a*1000000;}
+   return b*pow(10,4-UConversie);
+   if(UCurent==9&&UConversie<8)
+  {b=a*28,3495231;
+   return b*pow(10,4-UConversie);}
+   if(UCurent==10&&UConversie<8)
+  {b=a*453,59237;
+   return b*pow(10,4-UConversie);}
+   if(UCurent==8&&UConversie==9)
+    return a*35273,9619;
+   if(UCurent==8&&UConversie==10)
+    return a*2204,62262;
+   if(UCurent==9&&UConversie==8)
+    return a*283495231/pow(10,11);
+   if(UCurent==9&&UConversie==10)
+    return a*0,0625;
+   if(UCurent==10&&UConversie==8)
+    return a*45359237/pow(10,11);
+   if(UCurent==10&&UConversie==9)
+    return a*16;
+}
+
+
 int main()
 {
 Meniu();
